4a7295f57f5dedb8866e920bc314284133f8579e
Alex Johnson <alex.johnson293@gmail.com>
2022-11-15 14:11:35 -0800

    add async promise to the code and updated arguments passing multiple data points through response

--- src/index.js
+++ src/index.js
@@ -5,23 +5,26 @@ import './css/styles.css';
 // Business Logic
 
 function getWeather(city) {
-  let request = new XMLHttpRequest();
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}&units=imperial`;
+  let promise = new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}&units=imperial`;
+    request.addEventListener("loadend", function() {
+      const response = JSON.parse(this.responseText);
+      if (this.status === 200) {
+        resolve([response, city]);
+      } else {
+        reject([this, response, city]);
+      }
+    });
+    request.open("GET", url, true);
+    request.send();
+  });
 
-  request.addEventListener("loadend", function() {
-    const response = JSON.parse(this.responseText);
-    console.log(response);
-    try {
-      checkResp(this.status);
-    } catch(error) {
-      console.log((`${error}`))
-    }
-    if (this.status === 200) {
-      printElements(response, city);
-    }
-  })
-  request.open("GET", url, true);
-  request.send();
+  promise.then(function(weatherDataArray) {
+    printElements(weatherDataArray);
+  }, function(errorArray) {
+    printError(errorArray);
+  });
 }
 
 function timeCalc(api) {
@@ -30,29 +33,23 @@ function timeCalc(api) {
   return timeOut;
 }
 
-  // catch...the rest of it
+
 
 // UI Logic
 
-function checkResp(response) {
-  if(response !== 200) {
-    console.log(`bad response ${response}`);
-    throw new Error("bad HTTP request");
-  } else {
-    console.log("request successful");
-  }
-}
-
-function printElements(apiResponse, city) {
+function printElements(data) {
   const img  = document.createElement("img");
   img.setAttribute("src", "https://storage.needpix.com/rsynced_images/arrow-39450_1280.png");
   img.setAttribute("width", "20px");
-  img.style.transform = `rotate(${apiResponse.wind.deg}deg)`;
-  let time = timeCalc(apiResponse.timezone);
-  document.querySelector('#showResponse').innerText = `Its timezone is GMT ${time}. The humidity in ${city} is ${apiResponse.main.humidity}%.\n
-  The temperature in Fahrenheit is ${apiResponse.main.temp} degrees. Current conditions: ${apiResponse.weather[0].description}\n
-  Winds of ${apiResponse.wind.speed} mph with gusts of ${apiResponse.wind.gust} mph.`;
+  img.style.transform = `rotate(${data[0].wind.deg}deg)`;
+  let time = timeCalc(data[0].timezone);
+  document.querySelector('#showResponse').innerText = `Its timezone is GMT ${time}. The humidity in ${data[1]} is ${data[0].main.humidity}%.\n The temperature in Fahrenheit is ${data[0].main.temp} degrees. Current conditions: ${data[0].weather[0].description}\n
+  Winds of ${data[0].wind.speed} mph with gusts of ${data[0].wind.gust} mph.`;
   document.querySelector("#windarrow").appendChild(img);
+}
+
+function printError(error) {
+  document.querySelector('#showresponse').innerText = `There was an error accessing the weather data for ${error[2]}: ${error[0].status} ${error[0].statusText}: ${error[1].message}`;
 }
 
 function handleFormSubmission(event) {
