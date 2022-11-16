2db283721672be48f647addfbd081aeee3044cda
Alex Johnson <alex.johnson293@gmail.com>
2022-11-16 09:51:23 -0800

    fix files before npm build

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import WeatherService from './weather-service.js'
+import NationalityService from './js/nationality-service.js'
 
 // Business Logic
 
